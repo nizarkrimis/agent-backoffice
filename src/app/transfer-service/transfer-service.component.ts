5dfa803506de59a4fa697e33445b162be7a5ecd3
Ghada Ahl Mbarek <ghadaahlmbarek@gmail.com>
2024-01-05 13:37:57 +0000

    transfer-service changes

--- src/app/transfer-service/transfer-service.component.ts
+++ src/app/transfer-service/transfer-service.component.ts
@@ -12,7 +12,6 @@ export class TransferServiceComponent {
   public transferInfo: any;
   public beneficiaryInfo: any;
   public errorMessage: string='';
-  public verifyName: string='';
 
   public searchTransfer(): void {
     // Réinitialiser les données
@@ -37,19 +36,20 @@ export class TransferServiceComponent {
 
     } else {
       // Afficher un message d'erreur si la référence n'est pas trouvée
-      this.errorMessage = 'Reference not found';
+      this.errorMessage = 'Reference not found !';
     }
   }
 
+  public message = '';
+  public verifyName: string='';
+
+
   public validatePayment(): void {
-    // Simuler la validation du paiement du transfert
-    // Mettre à jour le solde de l'agent (simulé ici)
-    // Mettre à jour l'état du transfert à "payé"
-    // Générer un reçu de paiement (simulé ici)
-    // Vous devrez également mettre en œuvre la génération du CRE fin de journée, la mise à jour
-    // de l'état du transfert à "payé", et l'édition du reçu de paiement.
-    // Simuler la validation du paiement du transfert
-    // Vous pouvez obtenir le montant à partir des données du transfert    
+    if(this.transferInfo.beneficiaryFullName.toLowerCase()===this.verifyName.toLowerCase()){
+      this.message='Payement validated';
+    }else{
+      this.message='Payement not validated';
+    }
   }
 
 }
