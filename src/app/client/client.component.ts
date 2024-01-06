ef0e6a914d43cf2a1706c15a8d3a1e3cf62ea743
nizarkrimis <nizar_id@live.fr>
2024-01-06 01:14:22 +0100

    modifiy to much style and adjustment

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html',
@@ -13,9 +14,38 @@ export class ClientComponent {
     { id: 4, firstname: "Client", lastname: '4', cne: 'EE995569', email: 'client4@example.com', phone: "0600000004", balance: 1800, accountType: "Normale" },
     { id: 5, firstname: "Client", lastname: '5', cne: 'EE995570', email: 'client5@example.com', phone: "0600000005", balance: 2500, accountType: "Normale" },
   ];
-  
+  showAddClientModal: boolean = false;
 
   constructor(private router: Router) {}
+
+  openAddClientModal(): void {
+    this.showAddClientModal = true;
+  }
+  closeAddClientModal(): void {
+    this.showAddClientModal = false;
+  }
+  
+  addNewClient(newClient: any): void {
+    // Validate and save the new client
+    // Example: (you may need to perform additional validations)
+    if (newClient.firstname && newClient.lastname && newClient.email) {
+      this.clients.push({
+        id: this.clients.length + 1,
+        firstname: newClient.firstname,
+        lastname: newClient.lastname,
+        cne: newClient.cne,
+        email: newClient.email,
+        phone: newClient.phone,
+        balance: newClient.balance,
+        accountType: newClient.accountType,
+      });
+
+      // Close the modal after adding a new client
+      this.closeAddClientModal();
+
+      // You can add further logic such as resetting the form
+    }
+  }
 
   goToClientDetails(id: number): void {
     this.router.navigate(['/client', id]);
