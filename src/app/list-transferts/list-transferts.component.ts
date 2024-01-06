a27c6aa2fa82a65fd652c0fa485b4b0cb668c1af
nizarkrimis <nizar_id@live.fr>
2024-01-06 20:44:50 +0100

    add button to transfer

--- src/app/list-transferts/list-transferts.component.ts
+++ src/app/list-transferts/list-transferts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgComponentOutlet } from '@angular/common';
 import { TransfertsService } from '../service/transferts.service';
 
 @Component({
